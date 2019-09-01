1694a7abaeeb7d79c5f7e6692f5c5c7dda261603
AndreLPD <andre_luis_pessoa@hotmail.com>
2019-08-31 22:45:18 -0300

    Add list dots

--- js/carousel.js
+++ js/carousel.js
@@ -4,16 +4,39 @@
     const $carouselItens = document.querySelectorAll(".carousel__item");
     const $btnPrev = document.querySelector(".carousel__btn--prev");
     const $btnNext = document.querySelector(".carousel__btn--next");
+    const $carouselPaginacao = document.querySelector(".carousel__paginacao");
+    
     const $qtdBanners = $carouselItens.length;
     let bannerAtual = 0;
+    let paginacaoLi = true;
 
     $carousel.style.overflowX = "hidden";
-    $btnPrev.style.display = "block";
-    $btnNext.style.display = "block";
+    /*$btnPrev.style.display = "block";
+    $btnNext.style.display = "block";*/
 
     $btnPrev.addEventListener("click", showPrev);
     $btnNext.addEventListener("click", showNext);
 
+    if(paginacaoLi === true){
+        for (let i = 0; i < $qtdBanners; i++) {
+            let newElement = document.createElement("li");
+            newElement.classList.add("item__pag");
+            newElement.value = i;
+            $carouselPaginacao.append(newElement); 
+        }
+    }
+    const $carouselItemPag = document.querySelectorAll(".item__pag");
+    Array.prototype.forEach.call($carouselItemPag, function($itemLi){
+        $itemLi.addEventListener("click", mostraBannerPag);
+    });
+
+    function mostraBannerPag(){
+        var largura = getComputedStyle($carouselItens[0]).width;
+        largura = parseInt(largura);
+        var posTotal = largura*$carouselItens.length*-1;
+        var posAtual = this.value*largura*-1;
+        $carouselInner.style.transform = 'translateX('+posAtual+'px)';
+    }
     function showPrev(){
         bannerAtual--;
         mostraBanner(bannerAtual)
@@ -22,34 +45,30 @@
         bannerAtual++;
         mostraBanner(bannerAtual);
     }
-
-    function mostraBanner(bannerAtual){
-        var largura = getComputedStyle($carouselItens[0]).width;
-        largura = parseInt(largura);
-        var posicaoTotal = largura*$carouselItens.length*-1;
-        console.log("posicaoTotal",posicaoTotal);
-        var posicaoAtual = bannerAtual*largura*-1;
-        console.log("posicaoAtual",posicaoAtual);
-        
-
+    function setupNav(bannerAtual){
         if(bannerAtual>0){
             $btnPrev.disabled = false;
-        }
-        else{
-            $btnPrev.disabled = true;
+        }else{
+            bannerAtual = 0;
+            $btnPrev.disabled= true;
         }
         if(bannerAtual === $qtdBanners - 1){
             $btnNext.disabled = true;
         }else{
             $btnNext.disabled = false;
         }
-        if(posicaoAtual>posicaoTotal){
-            $carouselInner.style.transform = 'translateX('+posicaoAtual+'px)';
-        }
-            
-        
-
-        
     }
-
+    function mostraBanner(bannerAtual){
+        setupNav(bannerAtual);
+        var largura = getComputedStyle($carouselItens[0]).width;
+        largura = parseInt(largura);
+        var posicaoTotal = largura*$carouselItens.length*-1;
+        var posicaoAtual = bannerAtual*largura*-1;
+        
+        if(bannerAtual>=0){
+            if(posicaoAtual>posicaoTotal){
+                $carouselInner.style.transform = 'translateX('+posicaoAtual+'px)';
+            }
+        }
+    }
 })()
